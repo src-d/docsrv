7f4b61d40cb741166e811b7fded0bc190e9a275c
Miguel Molina <hi@mvader.me>
2017-05-25 15:15:32 +0200

    execute init scripts before generating default error pagins

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# run init scripts, if any
+for file in /etc/docsrv-init/*.sh; do
+        if [ -f $file ]; then
+                sh $file;
+        fi
+done
+
 # create 404 page if not exists
 if [ ! -f /var/www/public/errors/404.html ]; then
         echo '<h1>Not Found</h1>' > /var/www/public/errors/404.html;
@@ -9,12 +16,5 @@ fi
 if [ ! -f /var/www/public/errors/500.html ]; then
         echo '<h1>Server Error</h1>' > /var/www/public/errors/500.html;
 fi
-
-# run init scripts, if any
-for file in /etc/docsrv-init/*.sh; do
-        if [ -f $file ]; then
-                sh $file;
-        fi
-done
 
 /usr/bin/supervisord
